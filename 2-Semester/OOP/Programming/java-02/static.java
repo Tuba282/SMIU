7edab6d6df88e67ea252bd0e4ca23191a9485579
Tuba282 <tubajan282@gmail.com>
2026-03-26 02:55:50 +0500

    DLD slide added

---
+++ 2-Semester/OOP/Programming/java-02/static.java
@@ -0,0 +1,8 @@
+class Student {
+    String name; // Non-static
+    static String schoolName = "City School"; // Static variable
+
+    void display() {
+        System.out.println(name + " parhta hai " + schoolName + " mein.");
+    }
+}
